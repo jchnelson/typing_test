9dde776cacf8c332d6609f79f15789df7343e34a
Jakob Nelson <jakob.ch.nelson@gmail.com>
2023-07-16 14:56:24 -0600

    Word entry working typingtest_py updated

--- typing_test.py
+++ typing_test.py
@@ -3,7 +3,7 @@ around, and user must type them before they hit the edge to accumulate a score.
 '''
 import random, logging
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 logging.basicConfig(filename='typing_test.log', level=logging.DEBUG, 
                     format='%(asctime)s - %(levelname)s: %(message)s')
@@ -14,6 +14,7 @@ WORD_MARGIN = 20
 MOVE_STEPY = 10
 MOVE_STEPX = 25
 
+#up-vertical, left-horizontal, down-right-diagonal, etc
 PATHS = ('uvert', 'dvert', 'lhorz', 'rhorz',
           'urdiag', 'uldiag', 'drdiag','dldiag')
 PATH_INITIAL_POS = {
@@ -42,6 +43,10 @@ COLORS = ['red', 'blue', 'purple', 'cyan', 'black', 'yellow',
 
 active_words = {}
 next_word = 0
+player_score = 0
+gamespeed = 400
+score_barrier = 100
+interval = 10
 
 dict_file = open('dictionary.txt')
 dict_text = dict_file.read()
@@ -61,7 +66,9 @@ def create_test_base():
     testcanv.grid()
 
     test_entry_string = tk.StringVar()
-    testentry = ttk.Entry(main, textvariable=test_entry_string, width=40, background='dark gray')
+    widgets['test_entry_string'] = test_entry_string
+    testentry = tk.Entry(main, textvariable=test_entry_string, width=40,
+                         highlightthickness=3)
     widgets['testentry'] = testentry
     testentry.grid()
     testentry.focus_set()
@@ -74,6 +81,29 @@ def create_test_base():
     logging.debug('Board Initialized')
 
     return widgets
+
+def check_entry(event):
+    global player_score, score_barrier, gamespeed
+    testentry = widgets['testentry']
+    testcanv = widgets['testcanv']
+    entry_string = widgets['test_entry_string'].get()
+    word_list = list(active_words.keys())
+
+    if entry_string in word_list:
+        testcanv.delete(testcanv.find_withtag(entry_string)[0])
+        player_score += len(entry_string)
+        widgets['test_entry_string'].set('')
+        testentry['highlightcolor']= 'SystemWindowFrame'
+        if player_score >= score_barrier:
+            score_barrier += 100
+            gamespeed -= 50
+
+    else:
+        testentry['highlightcolor'] = 'red'
+    
+def clear_color(event):
+    widgets['testentry']['highlightcolor'] = 'SystemWindowFrame'
+
 
 def get_new_word():
     '''Choose a new word, create it on the canvas, and assign it a path'''
@@ -100,7 +130,7 @@ def get_new_word():
 def word_move():
     '''Move all active words to their next location according to path'''
     global next_word
-    
+
     for word in active_words:
         word_path = active_words[word][2]
         widgets['testcanv'].move(word, PATH_MOV[word_path][0], 
@@ -108,11 +138,11 @@ def word_move():
         logging.debug(f"Word {word} at {widgets['testcanv'].coords('word')}, count = {next_word}")
 
     next_word += 1
-    if next_word == 10:
+    if next_word == interval:
         get_new_word()
         next_word = 0
 
-    root.after(200, word_move)
+    root.after(gamespeed, word_move)
         
     
     # root.after(300, lambda: word_move(new_word))
@@ -123,9 +153,17 @@ def word_move():
 
 
 if __name__ == '__main__':
-
+    # messagebox.showinfo('Typing Test', '''Welcome to the Typing Test game!
+    #                 Enter the words as quickly as you can, and
+    #                 hit return. Go for a high score!''')
     root = tk.Tk()
+    # root.wm_attributes('-topmost', 1)
+    # root.focus_force()
     widgets = create_test_base()
+    widgets['testentry'].bind('<Return>', check_entry)
+    root.bind('<BackSpace>', clear_color)
+    widgets['testentry'].focus_set()
+
     get_new_word()
     word_move()
 
